6157c611f1a7a62efe88f25fd933acc3573ec37b
choseungyoon <cho31250@gmail.com>
2023-09-09 14:06:13 +0900

    앱 전체 가로 padding 적용

--- App.js
+++ App.js
@@ -47,6 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal:15,
     //paddingTop : statusBarHeight
     //alignItems: 'center',
     //justifyContent: 'center',
